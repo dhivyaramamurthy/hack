f71d31e4c86bb565569bfd5379a78dd914f24534
dhashrath <dhashrath@hotmail.com>
2014-07-24 07:38:40 +0530

    extracting mail manager class

---
+++ Hackathon/Hackathon/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace main
+{
+    class Logger
+    {
+        public static void WriteInformation(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Hackathon/Hackathon/MailManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Exchange.WebServices.Data;
+
+namespace main
+{
+    class MailManager
+    {
+        private ExchangeService _exchangeService;
+
+        public MailManager(string emailAddress)
+        {
+            _exchangeService = new ExchangeService(ExchangeVersion.Exchange2010_SP2);
+            _exchangeService.AutodiscoverUrl(emailAddress);
+        }
+
+        public void FindChildFolders()
+        {
+            FindFoldersResults findResults = _exchangeService.FindFolders(
+            WellKnownFolderName.Inbox,
+            new FolderView(int.MaxValue));
+
+            foreach (Folder folder in findResults.Folders)
+            {
+                Logger.WriteInformation(folder.DisplayName);
+                Logger.WriteInformation(folder.Id.ToString());
+            }
+        }
+
+        public void ListFirstTenItems()
+        {
+            FindItemsResults<Item> findResults = _exchangeService.FindItems(
+            WellKnownFolderName.Inbox,
+            new ItemView(10));
+
+            foreach (EmailMessage item in findResults.Items)
+            {
+                item.Load();
+                Logger.WriteInformation(item.Body);
+            }
+        }
+    }
+}
